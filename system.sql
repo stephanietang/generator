dd95c1d5274c4c18f001a22c06bd914ee808720f
Stephanie Tang <txj.stephanie@gmail.com>
2015-08-08 23:54:48 +0800

    add systemParam table instead of country/sex/degree/experience

--- system.sql
+++ system.sql
@@ -3,45 +3,31 @@ DROP TABLE IF EXISTS experience;
 DROP TABLE IF EXISTS degree;
 DROP TABLE IF EXISTS sex;
 
-CREATE TABLE country(
+CREATE TABLE SYSTEM_PARAM(
 	id INT NOT NULL AUTO_INCREMENT,
-	country_name VARCHAR(50),
-	country_name_eng VARCHAR(50),
+	param_group VARCHAR(10),
+	param_value INT,
+	param_desc_chi VARCHAR(50),
+	param_desc_eng VARCHAR(50),
+	enabled INT,
 	
-	PRIMARY KEY(id)
+	PRIMARY KEY(id),
+	INDEX system_param_idx (param_group)
 );
 
-INSERT INTO country VALUES (1, '中国', 'China');
-INSERT INTO country VALUES (2, '美国', 'USA');
 
-CREATE TABLE experience(
-	id INT NOT NULL AUTO_INCREMENT,
-	exp_desc VARCHAR(50),
-	
-	PRIMARY KEY(id)
-);
+INSERT INTO SYSTEM_PARAM VALUES (1, 'COUNTRY',1,'中国', 'China',1);
+INSERT INTO SYSTEM_PARAM VALUES (2, 'COUNTRY',2,'美国', 'USA',1);
 
-INSERT INTO experience VALUES (1, '应届毕业生');
-INSERT INTO experience VALUES (2, '一年至三年');
-INSERT INTO experience VALUES (3, '三年至五年');
-INSERT INTO experience VALUES (4, '五年以上');
+INSERT INTO SYSTEM_PARAM VALUES (3, 'EXPERIENCE',1, '应届毕业生','Graduating Student',1);
+INSERT INTO SYSTEM_PARAM VALUES (4, 'EXPERIENCE',2, '一年至三年', '1-3 Years Experience',1);
+INSERT INTO SYSTEM_PARAM VALUES (5, 'EXPERIENCE',3, '三年至五年', '3-5 Years Experience',1);
+INSERT INTO SYSTEM_PARAM VALUES (6, 'EXPERIENCE',4, '五年以上', 'Above 5 Years Experience',1);
 
-CREATE TABLE degree(
-	id INT NOT NULL AUTO_INCREMENT,
-	degree_desc VARCHAR(10),
-	PRIMARY KEY(id)
-);
+INSERT INTO SYSTEM_PARAM VALUES (7, 'DEGREE',1,'本科', 'Bachelor',1);
+INSERT INTO SYSTEM_PARAM VALUES (8, 'DEGREE',2,'硕士','Master',1);
+INSERT INTO SYSTEM_PARAM VALUES (9, 'DEGREE',3,'博士','PHD',1);
+INSERT INTO SYSTEM_PARAM VALUES (10, 'DEGREE',4,'大专','Associate Degree',1);
 
-INSERT INTO degree VALUES (1, '本科');
-INSERT INTO degree VALUES (2, '硕士');
-INSERT INTO degree VALUES (3, '博士');
-INSERT INTO degree VALUES (4, '大专');
-
-CREATE TABLE sex(
-	id INT NOT NULL AUTO_INCREMENT,
-	sex_desc VARCHAR(1),
-	PRIMARY KEY(id)
-);
-
-INSERT INTO sex VALUES (1, '男');
-INSERT INTO sex VALUES (2, '女');
+INSERT INTO SYSTEM_PARAM VALUES (11, 'SEX',1,'男','Male',1);
+INSERT INTO SYSTEM_PARAM VALUES (12, 'SEX',2,'女','Female',1);
