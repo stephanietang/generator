527f6b00bb9118220e51ceb7de2dce97ae5187f7
stephanie <tangxj@rthk.hk>
2016-05-18 16:42:34 +0800

    add work_exp table

--- talent.sql
+++ talent.sql
@@ -34,6 +34,20 @@ CREATE TABLE education(
 	CONSTRAINT fk_education FOREIGN KEY (basic_info_id) REFERENCES basic_info(id)
 );
 
+CREATE TABLE work_exp(
+	id INT NOT NULL AUTO_INCREMENT,
+	basic_info_id INT,
+	company_name VARCHAR(200),
+	start_year INT(4),
+	end_year INT(4),
+	position_name VARCHAR(200),
+	position_desc VARCHAR(500),
+	
+	PRIMARY KEY(id),
+	
+	CONSTRAINT fk_work_exp FOREIGN KEY (basic_info_id) REFERENCES basic_info(id)
+);
+
 CREATE TABLE avatar(
 	id INT NOT NULL AUTO_INCREMENT,
 	user_id INT NOT NULL,
@@ -51,3 +65,6 @@ CREATE TABLE avatar(
 insert into basic_info values (1, 1, 'abc', 1, 1, '16820428602', 1, 1, 1, 1985);
 insert into education values(1,1,'清华大学',1,2003,2007,'计算机科学与技术');
 insert into education values(2,1,'哥伦比亚大学',2,2007,2009,'计算机科学与技术');
+
+insert into work_exp values(1,1,'company1',2009,2011,'Java programmer', 'testing');
+insert into work_exp values(2,1,'company1',2011,2012,'Java senior programmer', 'testing');
